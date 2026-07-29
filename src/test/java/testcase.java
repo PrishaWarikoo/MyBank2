c99aca278de08b03a1a98182ee1c86b481ddc692
PrishaWarikoo <prisha.warikoo@oracle.com>
2026-07-29 12:24:57 +0530

    First Commit

---
+++ src/test/java/testcase.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class testcase {
+    @Test
+    public void testCaseOne() {
+        Assertions.assertTrue(true);
+        System.out.println("======= ");
+    }
+}
